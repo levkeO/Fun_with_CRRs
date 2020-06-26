542442cb4ebe1a86bff0df2d488fa1f175171ee8
Levke Ortlieb <levke.ortlieb@bristol.ac.uk>
2020-06-26 12:23:43 +0100

    for fractal dimension without gyration tensor
    Calculate radius of gyration for fractal dimension without
    tensor if gyration since unwrapping coordinates is incredibly slow
    Fixed the slow/fast bug as well

--- gyr_no_Ten.py
+++ gyr_no_Ten.py
@@ -1,3 +1,5 @@
+
+
 from ovito import dataset
 import numpy as np
 from ovito.vis import *
@@ -13,10 +15,8 @@ sys.path.append(path)
 from numba import njit, config, __version__
 from numba.extending import overload
 import facil_module as nf
-L = 19.25985167448
 cutoff= 1.3
 from numpy.linalg import eig
-import sys
 
 
 def set_cell(frame, data):
@@ -32,7 +32,7 @@ def set_cell(frame, data):
                 #set periodic boundary conditions
                 data.cell_.pbc = (True, True, True)
 
-#@njit
+#njit
 def gyration_tensor(_r, Print=False):
     N=_r.shape[0]
     # centre of mass
@@ -85,20 +85,37 @@ lag = int(sys.argv[3]) # time lag max dynamic heterogeneity in frames for chosen
 
 rho = 1.4                       # number density for periodic boundaries
 numFrames = int(sys.argv[5])
-numPart = 10002                 # number of particles
+numPart = int(sys.argv[6])                 # number of particles
 numFast = int(numPart*0.1)
 print(numFast)
 path2 = sys.argv[1]
 filexyz = sys.argv[2]           # coordinate file from command line
-side  = (numPart / rho)**(1/3)
-#narode = import_file(sys.argv[1]+sys.argv[2],multiple_frames=True,columns =["Particle Type", "Position.X", "Position.Y", "Position.Z"])
-
+L  = (numPart / rho)**(1/3)
+print(L)
 allCoords = nf.readCoords(path2+filexyz, numFrames,numPart)
 counter =0
 outFile ='fastPart_'+ filexyz[:-4] + '_temp.xyz'
 
 
+def radGyr(coords_RG,L):
+	"""
+	radius of gyration
+	"""
+	avDist = np.array([0.0,0.0,0.0])
+	for particle in range(1,len(coords_RG)):
+		dist = coords_RG[particle] - coords_RG[0]
+		dist = nf.periodic_boundary(dist,L)
+		avDist += dist/(len(coords_RG)-1)
+		avPos = coords_RG[0] + avDist
+		avPos = nf.periodic_boundary(avPos,L)
+		gyration = avDist[0]**2+avDist[1]**2+avDist[2]**2
+	for particle in range(1,len(coords_RG)):
+		dist = coords_RG[particle] - avPos
+		dist = nf.periodic_boundary(dist,L)
+		gyration+=dist[0]**2 +dist[1]**2 + dist[2]**2
 
+	return gyration
+	
 def partID(frame,data):
         """
         Assigns ID to every particle
@@ -113,12 +130,15 @@ num1 = []#np.zeros(len(range(lag,numFrames)))
 ds = []
 es = []
 fs =[]
-for frame in range(lag,lag+10,1):
+clusID =[]
+clusCount = []
+for frame in range(lag,numFrames,1):
 	print('frame: ',frame)
 	node = import_file(sys.argv[1]+sys.argv[2],multiple_frames=True,columns =["Particle Type", "Position.X", "Position.Y", "Position.Z"])
-	dist =np.array([nf.squareDist(allCoords[:,particle,:],frame-lag,frame,side) for particle in range(numPart)])
+	node.modifiers.append(set_cell)
+	dist =np.array([nf.squareDist(allCoords[:,particle,:],frame-lag,frame,L) for particle in range(numPart)])
 	data = node.compute(frame)
-	fastPart = dist.argsort()[:numFast]
+	fastPart = dist.argsort()[-numFast:]
 	ID = np.array(range(data.particles.count))
 	fast = np.zeros(data.particles.count)
 	node.modifiers.append(partID)
@@ -126,14 +146,13 @@ for frame in range(lag,lag+10,1):
 	for particle in ID:
 		if particle in fastPart:
 			fast[particle] = 1
-		#node = import_file(sys.argv[1]+sys.argv[2],multiple_frames=True,columns =["Particle Type", "Position.X", "Position.Y", "Position.Z"])
-	node.modifiers.append(set_cell)
 	node.modifiers.append(partID)
 	node.modifiers.append(ExpressionSelectionModifier(expression = 'fast ==1 '))
 	node.modifiers.append(ClusterAnalysisModifier(cutoff = cutoff,sort_by_size = True,only_selected = True))	
 	data = node.compute(frame)
+	#print('number of clusters: ',len(np.bincount(data.particles['Cluster'])))
 	largeClust = np.where(np.bincount(data.particles['Cluster'])>=5)[0]
-	print(largeClust[1:])
+	#print(largeClust[1:])
 	for clust in largeClust[1:]:
 		boolStr = 'Cluster =='+str(clust)
 		node.modifiers.append(ExpressionSelectionModifier(expression =boolStr))
@@ -141,31 +160,22 @@ for frame in range(lag,lag+10,1):
 		print('number of coordinates in cluster number ',clust, 'is ', sum(data.particles.selection.array))
 		coords_RG = data.particles['Position'].array
 		coords_RG = coords_RG[np.nonzero(data.particles.selection)]
-		node.modifiers.append(UnwrapTrajectoriesModifier())
-		data = node.compute(frame)
-		print('cluster: ',clust,'numPart',sum(data.particles.selection.array))
-		coord = data.particles['Position'].array
-		coord = coord[np.nonzero(data.particles.selection)]
-		a,b,c,R,d,e,f=gyration_tensor(coord)
-		gyr.append(R)
-		ds.append(d)
-		es.append(e)
-		fs.append(f)
-		num_gyr.append(len(coord))
-		avDist = np.array([0.0,0.0,0.0])
-		
-		for particle in range(1,len(coords_RG)):
-			dist = coords_RG[particle] - coord[0]
-			dist = nf.periodic_boundary(dist,L)
-			avDist += dist/(len(coords_RG)-1)
-			avPos = coords_RG[0] + avDist
-			avPos = nf.periodic_boundary(avPos,L)
-			gyration = avDist[0]**2+avDist[1]**2+avDist[2]**2
-		for particle in range(1,len(coords_RG)):
-			dist = coords_RG[particle] - avPos
-			dist = nf.periodic_boundary(dist,L)
-			gyration+=dist[0]**2 +dist[1]**2 + dist[2]**2
-		r_gyr.append(pl.sqrt(gyration/len(coord)))
-gyr=np.array(gyr).flatten()
+		#node.modifiers.append(UnwrapTrajectoriesModifier())
+		#data = node.compute(frame)
+		#print('cluster: ',clust,'numPart',sum(data.particles.selection.array))
+		#coord = data.particles['Position'].array
+		#coord = coord[np.nonzero(data.particles.selection)]
+		#a,b,c,R,d,e,f=gyration_tensor(coord)
+		#gyr.append(R)
+		#ds.append(d)
+		#es.append(e)
+		#fs.append(f)
+		#node.modifiers.append(WrapPeriodicImagesModifier())
+		clusCount.append(sum(data.particles.selection.array))
+		clusID.append(clust)
+		num_gyr.append(len(coords_RG))
+		gyration = radGyr(coords_RG,L)
+		r_gyr.append(pl.sqrt(gyration/len(coords_RG)))
+#gyr=np.array(gyr).flatten()
 T = sys.argv[4]
-pl.savetxt('r_gyr_num_T'+T+'_lag'+str(lag)+'_old.txt',np.real([num_gyr,gyr,r_gyr,ds,es,fs]))
+pl.savetxt('r_gyr_noTen_num_T'+T+'_lag'+str(lag)+'_N_'+str(numPart)+'.txt',np.real([num_gyr,r_gyr,clusID,clusCount]))
