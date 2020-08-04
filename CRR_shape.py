85a588fec6681119860b0c7c0c3149b67f0932f7
Levke Ortlieb <levke.ortlieb@bristol.ac.uk>
2020-08-04 12:21:16 +0100

    Rename file to indicate that looking back

--- CRR_shape.py
+++ CRR_shape.py
@@ -84,10 +84,10 @@ numNeighCl1 = []
 mobility = []
 clusdistr = []
 with open(outFile,'w') as outFile:
-	for frame in range(0,numFrames-lag,1):
+	for frame in range(lag,numFrames,1):
 		node = import_file(sys.argv[1]+sys.argv[2],multiple_frames=True,columns =["Particle Type", "Position.X", "Position.Y", "Position.Z"])
 		node.modifiers.append(set_cell)
-		dist =np.array([nf.squareDist(allCoords[:,particle,:],frame+lag,frame,L) for particle in range(numPart)])
+		dist =np.array([nf.squareDist(allCoords[:,particle,:],frame-lag,frame,L) for particle in range(numPart)])
 		mobility.append(dist)
 		fastPart = dist.argsort()[-numFast:]
 		fast = np.zeros(numPart)
